035c7a462b4d4ebaba08f2f1a2e09065965a7213
Carol Leão <51929598+CarolLeao@users.noreply.github.com>
2026-05-20 11:40:43 -0300

    Update Test_feeding_pressure.R
    instalar novo pacote

--- Test_feeding_pressure.R
+++ Test_feeding_pressure.R
@@ -2,6 +2,7 @@
 # Script para teste de disciplina da pós graduação com o objetico de demonstrar o processo de comit 
 
 install.packages("dplR")
+install.packages("ggplot2")
 
 #carregar pacotes
 library(dplR)
