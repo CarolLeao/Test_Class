8883610d947e2dec728898a72a48877b28b7564b
CarolLeao <caroline.leao.costa@gmail.com>
2026-05-20 10:51:49 -0300

    adicionar e carregar pacotes

---
+++ Test_feeding_pressure.R
@@ -0,0 +1,9 @@
+
+# Script para teste de disciplina da pós graduação com o objetico de demonstrar o processo de comit 
+
+install.packages("tidyverse")
+install.packages("ggplot2")
+
+#carregar pacotes
+library(tidyverse)
+library(ggplot2)
